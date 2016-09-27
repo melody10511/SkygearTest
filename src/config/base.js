ba14ea1046aa6a6028c4e49b3edf0129b8a4ad10
Minhyung Joo <Minhyung Joo>
2016-09-27 15:28:41 +0800

    Initialize with Skygear

---
+++ src/config/base.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Settings configured here will be merged into the final config object.
+export default {
+  'skygear': {
+    'endPoint': 'https://myapp.skygeario.com/',
+    'apiKey': 'dc0903fa85924776baa77df813901efc'
+  }
+}
+
